5a5bccce76e2bde261d1d4d994bfb8b486594a36
khan-shavez <shavezkhanit@gmail.com>
2025-08-02 15:50:41 +0530

    create router and authController function

--- server/controllers/authController.js
+++ server/controllers/authController.js
@@ -3,6 +3,7 @@ import jwt from "jsonwebtoken";
 import userModel from "../models/usermodel.js";
 import transporter from "../config/nodemailer.js";
 // import { text } from "express";
+// import { text } from "express";
 
 export const register = async (req, res) => {
   const { name, email, password } = req.body;
@@ -35,14 +36,21 @@ export const register = async (req, res) => {
     });
 
     // Sending welcome email
-    const mailoOptions = {
+    console.log("env email logged _", process.env.SENDER_EMAIL);
+    const mailOptions = {
       from: process.env.SENDER_EMAIL,
-      to: email,
-      subject: "Welcome to MernStack",
-      text: `Welcome to mernstack website. Your account has been created with email id: ${email}`,
+      to: email, // your own email
+      subject: "🔥 Welcome to MernStack",
+      text: `Welcome to MernStack website. Your account has been created with email id: ${email}`,
     };
 
-    await transporter.sendMail(mailoOptions);
+    await transporter.sendMail(mailOptions, (err, info) => {
+      if (err) {
+        console.log("❌ Error sending email:", err.message);
+      } else {
+        console.log("✅ Email sent successfully:", info.response);
+      }
+    });
 
     return res.json({ success: true });
   } catch (error) {
@@ -99,6 +107,167 @@ export const logout = (req, res) => {
     });
 
     return res.json({ success: true, message: "Logged Out" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+//  Send Verification OTP to the user's email
+
+export const sendVerifyOtp = async (req, res) => {
+  try {
+    if (!req.body || !req.body.userId) {
+      return res.json({ success: false, message: "Missing userId in request" });
+    }
+
+    const { userId } = req.body;
+
+    const user = await userModel.findById(userId);
+    if (!user) return res.json({ success: false, message: "User not found" });
+
+    if (user.isAccountVerified) {
+      return res.json({ success: false, message: "Account already verified" });
+    }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.verifyOtp = otp;
+    user.verifyOtpExpireAt = Date.now() + 24 * 60 * 60 * 1000;
+
+    await user.save();
+
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email, // your own email
+      subject: "Account Verification OTP",
+      text: `Your OTP is ${otp}. Verify your account using this OTP.`,
+    };
+
+    await transporter.sendMail(mailOption);
+
+    res.json({ success: true, message: "Verification OTP sent on Email" });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+export const verifyEmail = async (req, res) => {
+  const { userId, otp } = req.body;
+
+  if (!userId || !otp) {
+    return res.json({ success: false, message: "Missing Details" });
+  }
+
+  try {
+    const user = await userModel.findById(userId);
+
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    if (user.verifyOtp === "" || user.verifyOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.verifyOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expired" });
+    }
+
+    (user.isAccountVerified = true),
+      (user.verifyOtp = ""),
+      (user.verifyOtpExpireAt = 0);
+
+    await user.save();
+    return res.json({ success: true, message: "Email verified successfully" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// Check if user is Authrnticated
+
+export const isAuthenticated = async (req, res) => {
+  try {
+    return res.json({ success: true });
+  } catch (error) {
+    res.json({ success: false, message: error.message });
+  }
+};
+
+// Send Password reset OTP
+
+export const sendResetOtp = async (req, res) => {
+  const { email } = req.body;
+
+  if (!email) {
+    return res.json({ success: false, message: "Email is required" });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    const otp = String(Math.floor(100000 + Math.random() * 900000));
+
+    user.resetOtp = otp;
+    user.resetOtpExpireAt = Date.now() + 15 * 60 * 1000;
+
+    await user.save();
+
+    const mailOption = {
+      from: process.env.SENDER_EMAIL,
+      to: user.email, // your own email
+      subject: "Password Reset OTP",
+      text: `Your OTP for resetting your password is ${otp}. Use this OTP to proceed with resetting your password `,
+    };
+
+    await transporter.sendMail(mailOption);
+
+    return res.json({ success: true, message: "OTP sent to your email" });
+  } catch (error) {
+    return res.json({ success: false, message: error.message });
+  }
+};
+
+// Reset user password
+
+export const resetPassword = async (req, res) => {
+  const { email, otp, newPassword } = req.body;
+
+  if (!email || !otp || !newPassword) {
+    return res.json({
+      success: false,
+      message: "Email, OTP, and new password are required",
+    });
+  }
+
+  try {
+    const user = await userModel.findOne({ email });
+    if (!user) {
+      return res.json({ success: false, message: "User not found" });
+    }
+
+    if (user.resetOtp === "" || user.resetOtp !== otp) {
+      return res.json({ success: false, message: "Invalid OTP" });
+    }
+
+    if (user.resetOtpExpireAt < Date.now()) {
+      return res.json({ success: false, message: "OTP Expired" });
+    }
+
+    const hashedPassword = await bcrypt.hash(newPassword, 10);
+
+    user.password = hashedPassword;
+    user.resetOtp = "";
+    user.resetOtpExpireAt = 0;
+
+    await user.save();
+
+    return res.json({
+      success: true,
+      message: "Password has been reset successfully",
+    });
   } catch (error) {
     return res.json({ success: false, message: error.message });
   }
