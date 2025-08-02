d728054ae3f16661b34cba911ba5c61ef1fe7e3b
khan-shavez <shavezkhanit@gmail.com>
2025-08-02 20:19:03 +0530

    add login, home, reset password, email verify pages and navbar/header components

---
+++ client/src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import { assets } from '../assets/assets'
+
+const Header = () => {
+    return (
+        <div>
+            <img src={assets.header_img} alt='' className='w-36 h-36 rounded-full mb-6' />
+            <h1>Hey Developer <img className='w-8 aspect-square' src={assets.hand_wave} alt='' /></h1>
+        </div>
+    )
+}
+
+export default Header
